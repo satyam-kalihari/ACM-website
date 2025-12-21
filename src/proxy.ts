c22ce85eb1a359c6040ad87c9bf5947c97ba54bc
satyam-kalihari <satyamkalihari17@gmail.com>
2025-12-21 23:25:32 +0530

    middleware logic for admin

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,49 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+  createClerkClient,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-up(.*)", "/sign-in(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/admin/dashboard(.*)"]);
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+  const { isAuthenticated, redirectToSignIn, sessionClaims } = await auth();
+
+  if (!isAuthenticated && !isPublicRoute(req)) {
+    return redirectToSignIn();
+  }
+
+  if (isAuthenticated) {
+    try {
+      const userId = sessionClaims?.sub;
+      if (userId) {
+        const user = await clerkClient.users.getUser(userId);
+        const role = user.publicMetadata.role as string | undefined;
+  
+        //admin role redirection
+        if (role === "admin" && req.nextUrl.pathname == "/dashboard") {
+          return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+        }
+  
+        if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+          return NextResponse.redirect(new URL("/dashboard"));
+        }
+  
+        // redirect auth user trying to access public routes
+        if (isPublicRoute(req)) {
+          return NextResponse.redirect(
+            new URL(role === "admin" ? "/admin/dashboard" : "/dashboard", req.url)
+          );
+        }
+      }
+    } catch (error) {
+      console.error(error)
+      return NextResponse.redirect(new URL("/", req.url))
+    }
   }
 });
 
