bdb3a7b67db1865b639f2467e9e74bf24d577ace
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-05 00:56:40 +0530

    skipping retry method as Quota limit exceeded.�

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,43 +12,44 @@ const Dashboard = async () => {
     redirect("/sign-in");
   }
 
-  let retries = 0;
-  const maxRetries = 3;
+  // let retries = 0;
+  // const maxRetries = 3;
 
-  while (retries < maxRetries) {
-    let dbUser = null;
-    try {
-      dbUser = await getUserFromDB(clerkUser.id);
+  // while (retries < maxRetries) {
+  let dbUser = null;
+  try {
+    dbUser = await getUserFromDB(clerkUser.id);
 
-      if (!dbUser) {
-        const newDbUser = await createUser({
-          fullname: clerkUser.firstName || "" + " " + clerkUser.lastName || "",
-          email: clerkUser.emailAddresses[0].emailAddress,
-          clerkId: clerkUser.id,
-          avatar: clerkUser.imageUrl || "/default-avatar.png",
-          role: "user",
-          techStack: [],
-        });
+    if (!dbUser) {
+      const newDbUser = await createUser({
+        fullname: clerkUser.firstName || "" + " " + clerkUser.lastName || "",
+        email: clerkUser.emailAddresses[0].emailAddress,
+        clerkId: clerkUser.id,
+        avatar: clerkUser.imageUrl || "/default-avatar.png",
+        role: "user",
+        techStack: [],
+      });
 
-        dbUser = newDbUser.dbUser;
-      }
-    } catch (error) {
-      retries++;
-      if (error instanceof Error) {
-        console.log(error.message);
-      } else {
-        console.log("Error creating user", error);
-      }
-      return (
-        <div className="flex w-screen h-screen items-center justify-center">
-          <p className="text-red-500">
-            We are having trouble loading your profile. Please try again later.
-          </p>
-        </div>
-      );
+      dbUser = newDbUser.dbUser;
     }
-    await new Promise((resolve) => setTimeout(resolve, 1000 * retries));
+  } catch (error) {
+    // retries++;
+    // console.log(retries);
+    if (error instanceof Error) {
+      console.log(error.message);
+    } else {
+      console.log("Error creating user", error);
+    }
+    return (
+      <div className="flex w-screen h-screen items-center justify-center">
+        <p className="text-red-500">
+          We are having trouble loading your profile. Please try again later.
+        </p>
+      </div>
+    );
   }
+  //   await new Promise((resolve) => setTimeout(resolve, 1000 * retries));
+  // }
 
   return (
     <Suspense
