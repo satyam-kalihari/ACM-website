21dd4466988a424ef84c15fc9e293d69c950771b
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-05 23:11:22 +0530

    retrive while loop

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,44 +12,47 @@ const Dashboard = async () => {
     redirect("/sign-in");
   }
 
-  // let retries = 0;
-  // const maxRetries = 3;
+  let retries = 0;
+  const maxRetries = 3;
 
-  // while (retries < maxRetries) {
-  let dbUser = null;
-  try {
-    dbUser = await getUserFromDB(clerkUser.id);
+  while (retries < maxRetries) {
+    let dbUser = null;
+    try {
+      dbUser = await getUserFromDB(clerkUser.id);
+      if (dbUser) {
+        break;
+      }
 
-    if (!dbUser) {
-      const newDbUser = await createUser({
-        fullname: clerkUser.firstName || "" + " " + clerkUser.lastName || "",
-        email: clerkUser.emailAddresses[0].emailAddress,
-        clerkId: clerkUser.id,
-        avatar: clerkUser.imageUrl || "/default-avatar.png",
-        role: "user",
-        techStack: [],
-      });
+      if (!dbUser) {
+        const newDbUser = await createUser({
+          fullname: clerkUser.firstName || "" + " " + clerkUser.lastName || "",
+          email: clerkUser.emailAddresses[0].emailAddress,
+          clerkId: clerkUser.id,
+          avatar: clerkUser.imageUrl || "/default-avatar.png",
+          role: "user",
+          techStack: [],
+        });
 
-      dbUser = newDbUser.dbUser;
+        dbUser = newDbUser.dbUser;
+      }
+    } catch (error) {
+      retries++;
+      console.log(retries);
+      if (error instanceof Error) {
+        console.log(error.message);
+      } else {
+        console.log("Error creating user", error);
+      }
+      return (
+        <div className="flex w-screen h-screen items-center justify-center">
+          <p className="text-red-500">
+            We are having trouble loading your profile. Please try again later.
+          </p>
+        </div>
+      );
     }
-  } catch (error) {
-    // retries++;
-    // console.log(retries);
-    if (error instanceof Error) {
-      console.log(error.message);
-    } else {
-      console.log("Error creating user", error);
-    }
-    return (
-      <div className="flex w-screen h-screen items-center justify-center">
-        <p className="text-red-500">
-          We are having trouble loading your profile. Please try again later.
-        </p>
-      </div>
-    );
+    await new Promise((resolve) => setTimeout(resolve, 1000 * retries));
   }
-  //   await new Promise((resolve) => setTimeout(resolve, 1000 * retries));
-  // }
 
   return (
     <Suspense
