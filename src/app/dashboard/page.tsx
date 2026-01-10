ef30d98532927e8e73dcdd74bdac6fc334d68bf4
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-10 23:03:43 +0530

    Dashboard wireframe ready

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,14 +4,22 @@ import { redirect } from "next/navigation";
 import { createUser } from "@/lib/db/createUser";
 import { getUserFromDB } from "@/lib/db/getUser";
 import { Spinner } from "@/components/ui/spinner";
-import { platform } from "os";
-import { string } from "zod";
+import Leaderboard from "@/components/Leaderboard";
+import { Book, Star } from "lucide-react";
+
 interface PlatformCard {
   platform: string;
   heading: string;
   value: number;
   point: number;
   icon: string;
+}
+
+interface githubReoDetail {
+  reponame: string;
+  stared: boolean;
+  language: string;
+  repoLink: string;
 }
 
 const Dashboard = async () => {
@@ -21,6 +29,33 @@ const Dashboard = async () => {
     { platform: "leetcode", heading: "medium", value: 15, point: 30, icon: "" },
     { platform: "leetcode", heading: "easy", value: 43, point: 34, icon: "" },
     { platform: "github", heading: "stars", value: 43, point: 3, icon: "" },
+  ];
+
+  const githubRepos: githubReoDetail[] = [
+    {
+      reponame: "ACM-website",
+      stared: true,
+      language: "TypeScript",
+      repoLink: "",
+    },
+    {
+      reponame: "DSA",
+      stared: false,
+      language: "Java",
+      repoLink: "",
+    },
+    {
+      reponame: "clerk-practice-1",
+      stared: true,
+      language: "TypeScript",
+      repoLink: "",
+    },
+    {
+      reponame: "computer-network-college",
+      stared: false,
+      language: "C",
+      repoLink: "",
+    },
   ];
 
   const clerkUser = await currentUser();
@@ -83,18 +118,60 @@ const Dashboard = async () => {
         <div id="total_point"></div>
       </section>
       <section id="content_section">
+        {/* content_left */}
         <div id="content_left">
-          <div id="platform_cards" className=" grid grid-cols-2 sm:grid-cols-4 gap-1.5 md:gap-2.5">
+          {/* platform_cards */}
+          <div
+            id="platform_cards"
+            className=" grid grid-cols-2 sm:grid-cols-4 gap-1.5 md:gap-2.5"
+          >
             {platformCardsDetail.map((card) => (
               <div
                 key={card.heading}
-                className="w-[140] md:w-40 h-20 bg-amber-100 m-2 rounded-2xl mx-auto"
+                className="w-[140] md:w-40 h-20 bg-amber-100 m-2 rounded-md mx-auto"
               ></div>
             ))}
           </div>
-          <div id="leaderboard"></div>
+
+          {/* leaderboard */}
+          <div id="leaderboard">
+            <Leaderboard />
+          </div>
         </div>
-        <div id="content_right"></div>
+
+        {/* content_right */}
+        <div
+          id="content_right"
+          className="min-w-fit max-w-2xl mt-5 sm:px-2.5 bg-amber-700 sm:mx-auto"
+        >
+          {/* github_card */}
+          <div
+            id="github_card"
+            className="w-80 h-80 bg-amber-300 mx-auto"
+          ></div>
+          {/* github_toggle */}
+          <div
+            id="github_toggle"
+            className="my-4 bg-blue-500 w-80 h-13 mx-auto"
+          ></div>
+          {/* repos */}
+          <div id="repos" className="flex flex-col items-center gap-2">
+            {githubRepos.map((repo, key) => (
+              <div
+                key={key}
+                id="repo_card"
+                className="bg-emerald-500 w-80 h-11 p-2"
+              >
+                <div className="flex justify-between">
+                  <div className="flex">
+                    <Book /> <h2 className="ml-2">{repo.reponame}</h2>
+                  </div>
+                  <Star fill={repo.stared? "white" : "none"} className="left-0" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </section>
     </Suspense>
   );
