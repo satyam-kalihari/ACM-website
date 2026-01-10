0e4dfa6ee01be368d7401e1ce29f58caf7defbac
satyam-kalihari <satyamkalihari17@gmail.com>
2026-01-11 01:25:56 +0530

    basic dashboard ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,10 +5,10 @@ import { createUser } from "@/lib/db/createUser";
 import { getUserFromDB } from "@/lib/db/getUser";
 import { Spinner } from "@/components/ui/spinner";
 import Leaderboard from "@/components/Leaderboard";
-import { Book, Star } from "lucide-react";
+import { AlignEndHorizontal, BadgeQuestionMark, Book, FolderGit, FolderGit2, Star } from "lucide-react";
 
 interface PlatformCard {
-  platform: string;
+  platform: "leetcode" | "github";
   heading: string;
   value: number;
   point: number;
@@ -117,9 +117,9 @@ const Dashboard = async () => {
         <div id="hello_message">Ready to rock {clerkUser.firstName} ?</div>
         <div id="total_point"></div>
       </section>
-      <section id="content_section">
+      <section id="content_section" className="lg:flex">
         {/* content_left */}
-        <div id="content_left">
+        <div id="content_left" className="sm:w-3/4">
           {/* platform_cards */}
           <div
             id="platform_cards"
@@ -128,8 +128,46 @@ const Dashboard = async () => {
             {platformCardsDetail.map((card) => (
               <div
                 key={card.heading}
-                className="w-[140] md:w-40 h-20 bg-amber-100 m-2 rounded-md mx-auto"
-              ></div>
+                className="w-[140] md:w-40 h-20 m-2 rounded-md mx-auto bg-cover bg-[url('/images/test_bg.png')]"
+              >
+                <div
+                  className={`h-full w-full p-1 sm:pl-2 sm:pt-2 ${
+                    card.platform == "leetcode"
+                      ? "from-yellow-400/30 to-yellow-50/30"
+                      : "from-fuchsia-400/30 to-red-100/30"
+                  } bg-linear-to-br rounded-md backdrop-blur-xs border ${
+                    card.platform == "leetcode"
+                      ? "border-yellow-500/50"
+                      : "border-fuchsia-600/50"
+                  }`}
+                >
+                  <div
+                    id="platform_card_header"
+                    className="flex justify-between items-center"
+                  >
+                    <div className="flex gap-4 items-center">
+                      <div id="platform_card_icon">
+                        {card.platform == "leetcode" ? (
+                          <AlignEndHorizontal color="#ecdd79" />
+                        ) : (
+                          // <BadgeQuestionMark color="#ecdd79" />
+                          <FolderGit color="#d979ec" />
+                        )}
+                      </div>
+                      <div id="platform_card_header_text">{card.heading}</div>
+                    </div>
+                    <div id="platform_card_value" className="mx-1">
+                      {card.value}
+                    </div>
+                  </div>
+                  <div
+                    id="platform_card_points"
+                    className="mx-1 flex justify-center items-center font-bold text-3xl m-auto "
+                  >
+                    {card.point}
+                  </div>
+                </div>
+              </div>
             ))}
           </div>
 
@@ -140,33 +178,42 @@ const Dashboard = async () => {
         </div>
 
         {/* content_right */}
-        <div
-          id="content_right"
-          className="min-w-fit max-w-2xl mt-5 sm:px-2.5 bg-amber-700 sm:mx-auto"
-        >
+        <div id="content_right" className="min-w-fit mt-5 sm:px-2.5 sm:mx-auto">
           {/* github_card */}
           <div
             id="github_card"
-            className="w-80 h-80 bg-amber-300 mx-auto"
+            className="w-70 h-70 mx-auto p-1 sm:pl-2 sm:pt-2
+                       sm:rounded-md backdrop-blur-xs border border-white/30"
           ></div>
           {/* github_toggle */}
           <div
             id="github_toggle"
-            className="my-4 bg-blue-500 w-80 h-13 mx-auto"
-          ></div>
+            className="mb-2 mt-7 w-40 h-10 b sm:rounded-sm  border border-fuchsia-600/50 relative flex"
+          >
+            <div className="w-1/2 flex justify-center items-center">
+              <FolderGit2 size={18} />
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+              <Star size={18} />
+            </div>
+            <div className="w-1/2 absolute left-0 bg-fuchsia-400/20 sm:rounded-sm backdrop-blur-xs -z-10 h-full"></div>
+          </div>
           {/* repos */}
           <div id="repos" className="flex flex-col items-center gap-2">
             {githubRepos.map((repo, key) => (
               <div
                 key={key}
                 id="repo_card"
-                className="bg-emerald-500 w-80 h-11 p-2"
+                className="border border-white/30 sm:rounded-sm w-80 sm:w-95 h-11 p-2"
               >
                 <div className="flex justify-between">
                   <div className="flex">
                     <Book /> <h2 className="ml-2">{repo.reponame}</h2>
                   </div>
-                  <Star fill={repo.stared? "white" : "none"} className="left-0" />
+                  <Star
+                    fill={repo.stared ? "white" : "none"}
+                    className="left-0"
+                  />
                 </div>
               </div>
             ))}
